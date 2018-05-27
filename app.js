da7dfd3f9ed77384c2c9d517133ea15d6711e89d
doraeball22 <anurak_b@cmu.ac.th>
2018-05-27 15:51:14 +0700

    [3.Node fundamental Part-3] use yargs to parse your arguments.

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ console.log('Starting app2-command-argument.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+//let command = process.argv[2];
+let command = argv._[0];
 console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-  console.log('Adding new note');
+    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
+
 } else if (command === 'list') {
-  console.log('Listing all notes');
+    console.log('Listing all notes');
+    notes.getAll();
+
 } else if (command === 'read') {
-  console.log('Reading note');
+    console.log('Reading note');
+    notes.getNote(argv.title);
+
 } else if (command == 'remove') {
-  console.log('Removing note');
+    console.log('Removing note');
+    notes.removeNote(argv.title);
+
 } else {
-  console.log('Command not recognized');
+    console.log('Command not recognized');
 }
